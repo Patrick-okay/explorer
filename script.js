d6ee1a1dc685f5fb51741c6e3d3f63a96d9f8423
Patrick-okay <134115986+Patrick-okay@users.noreply.github.com>
2023-05-20 16:23:02 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
