b077c190f2e40e7b2725342368e8a402974f10bc
Patrick-okay <134115986+Patrick-okay@users.noreply.github.com>
2023-05-20 18:24:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+// Get the necessary elements
 const body = document.querySelector('body');
+const darkModeButton = document.getElementById('dark-mode-button');
 
-function toggleDarkMode() {
-  body.classList.toggle('dark-mode');
+// Check if dark mode is enabled in local storage
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+// Set the initial mode based on the value in local storage
+if (isDarkMode) {
+  enableDarkMode();
 }
 
-// Example usage of the toggleDarkMode function
-const darkModeButton = document.getElementById('dark-mode-button');
+// Function to enable dark mode
+function enableDarkMode() {
+  body.classList.add('dark-mode');
+  localStorage.setItem('darkMode', 'true');
+}
+
+// Function to disable dark mode
+function disableDarkMode() {
+  body.classList.remove('dark-mode');
+  localStorage.setItem('darkMode', 'false');
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  if (body.classList.contains('dark-mode')) {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+}
+
+// Add event listener to the dark mode button
 darkModeButton.addEventListener('click', toggleDarkMode);
